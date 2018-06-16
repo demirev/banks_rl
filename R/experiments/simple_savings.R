1441d1fbc33a4d38f85cf6d89f8a1929d9b577d3
demirev <demirev15@gmail.com>
2018-06-16 13:24:25 +0200

    experiment scirpt

---
+++ R/experiments/simple_savings.R
@@ -0,0 +1,37 @@
+source("R/setup.R")
+source("R/utils.R")
+source("R/agents/banks.R")
+source("R/agents/households.R")
+source("R/environments/simple_savings.R")
+source_python("python/dqn.py")
+
+Banks <- lapply(
+  1:3, 
+  function(i) DummyBank$new(rating = sample(1:5,1))
+)
+
+Households <- lapply(
+  1:10,
+  function(i) VanillaHousehold$new(length(Banks))
+)
+
+DQNs <- list(
+  withdraw = list(
+    current = DQN(24L, 4L), #$cuda(),
+    target  = DQN(24L, 4L)
+  ),
+  deposit = list(
+    current = DQN(24L, 4L),
+    target  = DQN(24L, 4L)
+  )
+)
+
+Economy <- SimpleSavings$new(
+  households = Households, 
+  banks = Banks, 
+  dqns = DQNs, 
+  lossFunction = compute_td_loss, 
+  bufferSize = 1000L
+)
+
+Economy$train(numEpisodes = 1024)
