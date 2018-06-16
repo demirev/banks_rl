a14dacea5fde33a9ecf0075db02df33984672712
demirev <demirev15@gmail.com>
2018-06-16 13:24:07 +0200

    removed done

--- python/dqn.py
+++ python/dqn.py
@@ -7,7 +7,7 @@ import torch.optim as optim
 import torch.autograd as autograd 
 import torch.nn.functional as F
 
-USE_CUDA = torch.cuda.is_available()
+USE_CUDA = False#torch.cuda.is_available()
 Variable = lambda *args, **kwargs: autograd.Variable(*args, **kwargs).cuda() if USE_CUDA else autograd.Variable(*args, **kwargs)
 
 from collections import deque
@@ -16,17 +16,17 @@ class ReplayBuffer(object):
     def __init__(self, capacity):
         self.buffer = deque(maxlen=capacity)
     
-    def push(self, state, action, reward, next_state, done):
+    def push(self, state, action, reward, next_state):
         state      = np.expand_dims(state, 0)
         next_state = np.expand_dims(next_state, 0)
             
-        self.buffer.append((state, action, reward, next_state, done))
+        self.buffer.append((state, action, reward, next_state))
     
     def sample(self, batch_size):
-        state, action, reward, next_state, done = zip(*random.sample(self.buffer, batch_size))
-        return np.concatenate(state), action, reward, np.concatenate(next_state), done
+        state, action, reward, next_state = zip(*random.sample(self.buffer, batch_size))
+        return np.concatenate(state), action, reward, np.concatenate(next_state)
     
-    def __len__(self):
+    def getLen(self):
         return len(self.buffer)
         
 class DQN(nn.Module):
@@ -34,12 +34,14 @@ class DQN(nn.Module):
         super(DQN, self).__init__()
         
         self.layers = nn.Sequential(
-            nn.Linear(env.observation_space.shape[0], 128),
+            nn.Linear(num_inputs, 128),
             nn.ReLU(),
             nn.Linear(128, 128),
             nn.ReLU(),
-            nn.Linear(128, env.action_space.n)
+            nn.Linear(128, num_actions)
         )
+        
+        self.outsize = num_actions
         
     def forward(self, x):
         return self.layers(x)
@@ -50,25 +52,24 @@ class DQN(nn.Module):
             q_value = self.forward(state)
             action  = q_value.max(1)[1].data[0].item()
         else:
-            action = random.randrange(env.action_space.n)
+            action = random.randrange(self.outsize)
         return action
         
-def compute_td_loss(batch_size, current_model, target_model, optimizer, beta):
-    state, action, reward, next_state, done = replay_buffer.sample(batch_size)
+def compute_td_loss(batch_size, replay_buffer, current_model, target_model, optimizer, beta):
+    state, action, reward, next_state = replay_buffer.sample(batch_size)
 
     state      = Variable(torch.FloatTensor(np.float32(state)))
     next_state = Variable(torch.FloatTensor(np.float32(next_state)))
     action     = Variable(torch.LongTensor(action))
     reward     = Variable(torch.FloatTensor(reward))
-    done       = Variable(torch.FloatTensor(done))
 
     q_values      = current_model(state)
     next_q_values = current_model(next_state)
-    next_q_state_values = target_model(next_state) 
-
+    next_q_state_values = target_model(next_state)
+    
     q_value       = q_values.gather(1, action.unsqueeze(1)).squeeze(1) 
     next_q_value = next_q_state_values.gather(1, torch.max(next_q_values, 1)[1].unsqueeze(1)).squeeze(1)
-    expected_q_value = reward + beta * next_q_value * (1 - done)
+    expected_q_value = reward + beta * next_q_value
     
     loss = (q_value - Variable(expected_q_value.data)).pow(2).mean()
         
@@ -78,3 +79,5 @@ def compute_td_loss(batch_size, current_model, target_model, optimizer, beta):
     
     return loss
     
+def update_target(current_model, target_model):
+    target_model.load_state_dict(current_model.state_dict())
