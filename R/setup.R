8e387867a8929edb637ab65d1dd42437144ca036
demirev <demirev15@gmail.com>
2018-07-20 11:06:08 +0300

    torch explicit import

--- R/setup.R
+++ R/setup.R
@@ -3,3 +3,9 @@ library(R6)
 library(reticulate)
 
 use_virtualenv("banks_abm")
+
+torch <- import("torch")
+nn <- import("torch.nn")
+optim <- import("torch.optim")
+autograd <- import("torch.autograd")
+F <- import("torch.nn.functional")
